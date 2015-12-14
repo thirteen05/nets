d7fad3176eedb4087422f7145e14f2dadf857c28
Justin R <justin@thirteen05.com>
2015-12-14 15:18:56 -0500

    Theme Set Up

---
+++ nets/riffraff.php
@@ -0,0 +1,21 @@
+<?php
+
+$riffQuotes = array(
+  "Never explain yourself to haters because they don't care, they just want to gather info to use against you.",
+  "Most people just wanna spend time with their family, but I just wanna sip drank and win a Grammy.",
+  "I could've played for the Vancouver Super Models, but they found codeine in my water bottles.",
+  "They don't know that I am have dinosaur; I come from the ancient lands, so I have a little more intelligence.",
+  "I would go on vacation, but my whole life is a weekend.",
+  "Don't talk about my past tense unless we're going camping.",
+  "I will break your hating ass down like a common denominator.",
+  "I'm like a Versace sponge: I soak up the best.",
+  "Suckers wanna hate me, librarians wanna date me.",
+  "The syrup in my soda got my brain doing yoga."
+  
+);
+
+$random = rand(1,10);
+
+?>
+
+<h3 style="color: #FFF"><?php echo $riffQuotes[$random]; ?></h3>
